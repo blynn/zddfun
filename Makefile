e93a4e8b00157c7b451c06ffd2146e466c349b49
Ben Lynn <benlynn@gmail.com>
2009-05-24 14:12:49 -0700

    ZDD for domino tilings of a chessboard.

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 CFLAGS:=-lgmp -Wall -std=gnu99 -O2 -ltcmalloc
 
 zdd: zdd.c cbt.c darray.c
+
+tri: tri.c cbt.c darray.c
--- tri.c
+++ tri.c
@@ -1,3 +1,4 @@
+// How many ways can you tile a chessboard with 1-, 2- and 3-polyonominos?
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,6 +16,7 @@ typedef struct node_s node_t[1];
 node_t pool[1<<24];
 uint32_t freenode, POOL_MAX = (1<<24) - 1;
 mpz_ptr count[1<<24];
+uint32_t vmax;
 
 void set_node(uint32_t n, uint16_t v, uint32_t lo, uint32_t hi) {
   pool[n]->v = v;
@@ -51,85 +53,37 @@ void get_count(uint32_t n) {
   gmp_printf("%d: %Zd\n", n, count[n]);
 }
 
-// Construct ZDD of sets containing exactly 1 digit at for all boxes
-// (r, c), starting at pool entry d.
-//
-// The ZDD begins:
-//   1 ... 2
-//   1 --- 10
-//   2 ... 3
-//   2 --- 10
-//   ...
-//   9 ... F
-//   9 --- 10
-//
-// and repeats every 10 levels:
-//   10 ... 11
-//   10 --- 19
-// and so on until 729 --- F, 729 ... T.
-//
-// This ZDD has 9^81 members.
-void global_one_digit_per_box() {
-  int d = freenode;
-  int next = 9;
-  for (int i = 1; i <= 729; i++) {
-    set_node(d, i, (i % 9) ? d + 1 : 0, next < 729 ? freenode + next : 1);
-    if (!(i % 9)) next += 9;
-    d++;
-  }
-  freenode = d;
-}
-
-// Construct ZDD of sets containing exactly 1 occurrence of digit d in row r.
-// For instance, if d = 3, r = 0:
-//
-//   1 === 2 === 3
-//   3 ... 4a, --- 4b
-//   4a === 5a === ... === 9a === ... === 11a === 12
-//   4b === 5b === ... === 9b === ... === 11b === 13b
-//   12 ... 13a, --- 13b
-//   13a === ... === 20a === 21
-//   13b === ... === 20b === 22b
-// and so on until:
-//   74a === 75 ... F, --- 76
-//   74b === 76
-//   76 === ... === 729 === T
-//
-// This ZDD has 9*2^720 members.
-// When intersected with the one-digit-per-box set, the result has
-// 9*8^8*9^72 members. (Pick 1 of 9 positions for d, leaving 8 possible choices
-// for the remaining 8 boxes in that row. The other 81 - 9 boxes can contain
-// any single digitS.)
-// The intersection for all d and a fixed r has 9!*9^72 members.
-// The intersection for all r and a fixed d has 9^9*8^72 members.
-void unique_digit_per_row(int d, int r) {
-  // The order is determined by sorting by number, then by letter.
+// Construct ZDD of sets containing exactly 1 of the elements in the given list.
+void contains_exactly_one(darray_t a) {
   int n = freenode;
-  int next = 81 * r + d;  // The next node involving the digit d.
   int v = 1;
-  int state = 0;
-  while (v <= 729) {
-    if (v == next) {
-      next += 9;
-      state++;
-      if (state == 1) {
-	// The first split in the ZDD.
-	set_node(n, v, n + 1, n + 2);
-	n++;
-      } else if (state < 9) {
-	// Fix previous node. We must not have a second occurrence of d.
-	set_node(n - 1, v - 1, n + 2, n + 2);
-	// If this is the first occurrence of d, we're on notice.
-	set_node(n, v, n + 1, n + 2);
-	n++;
-      } else {
-	// If we never saw d, then branch to FALSE.
-	// Otherwise reunite the branches.
-	set_node(n, v, 0, n + 1);
-	n++;
-	next = -1;
+  int i = 0;
+  while(v <= vmax) {
+    if (i >= darray_count(a)) {
+      set_node(n, v, n + 1, n + 1);
+      n++;
+    } else if (v == (int) darray_at(a, i)) {
+      // Find length of consecutive sequence.
+      int k;
+      for(k = 0;
+	  i + k < darray_count(a) && v + k == (int) darray_at(a, i + k); k++);
+      uint32_t h = v + k > vmax ? 1 : n + k + (darray_count(a) != i + k);
+      if (i >= 1) {
+	// In the middle of the list: must fix previous node too.
+	set_node(n - 1, v - 1, h, h);
       }
-    } else if (state == 0 || state == 9) {
+      i += k;
+      k += v;
+      while (v < k) {
+	set_node(n, v, n + 1, h);
+	v++;
+	n++;
+      }
+      v--;
+      if (darray_count(a) == i) {
+	set_node(n - 1, v, 0, h);
+      }
+    } else if (!i) {
       set_node(n, v, n + 1, n + 1);
       n++;
     } else {
@@ -140,58 +94,7 @@ void unique_digit_per_row(int d, int r) {
     }
     v++;
   }
-  // Fix last nodes.
-  if (pool[n - 2]->lo >= n) pool[n - 2]->lo = 1;
-  if (pool[n - 2]->hi >= n) pool[n - 2]->hi = 1;
-  if (pool[n - 1]->lo >= n) pool[n - 1]->lo = 1;
-  if (pool[n - 1]->hi >= n) pool[n - 1]->hi = 1;
-  freenode = n;
-}
-
-// Construct ZDD of sets containing exactly 1 of the elements in the given list
-// The list is terminated by -1. Only valid when there are no consecutive
-// elements. In particular, will not work for a 9 followed by a 1!
-// Generalization of previous function.
-void contains_exactly_one(int *list) {
-  // The order is determined by sorting by number, then by letter.
-  int n = freenode;
-  int v = 1;
-  int *next = list;
-  while (v <= 729) {
-    if (v == *next) {
-      next++;
-      if (list + 1 == next) {
-	// The first split in the ZDD.
-	set_node(n, v, n + 1, n + 2);
-	n++;
-      } else if (*next >= 1) {
-	// One of the boxes in the middle the list.
-	// Fix previous node.
-	set_node(n - 1, v - 1, n + 2, n + 2);
-	// If this is the first occurrence, we're on notice.
-	set_node(n, v, n + 1, n + 2);
-	n++;
-      } else {
-	// Last box in the list: -1 == *next.
-	// If we never saw anything from the list, then branch to FALSE.
-	// Otherwise reunite the branches.
-	set_node(n, v, 0, n + 1);
-	n++;
-      }
-    } else if (list == next || *next == -1) {
-      set_node(n, v, n + 1, n + 1);
-      n++;
-    } else {
-      set_node(n, v, n + 2, n + 2);
-      n++;
-      set_node(n, v, n + 2, n + 2);
-      n++;
-    }
-    v++;
-  }
-  // Fix last nodes.
-  if (pool[n - 2]->lo >= n) pool[n - 2]->lo = 1;
-  if (pool[n - 2]->hi >= n) pool[n - 2]->hi = 1;
+  // Fix last node.
   if (pool[n - 1]->lo >= n) pool[n - 1]->lo = 1;
   if (pool[n - 1]->hi >= n) pool[n - 1]->hi = 1;
   freenode = n;
@@ -218,26 +121,6 @@ void contains_all(int *list) {
   if (pool[n - 1]->lo == n) pool[n - 1]->lo = 1;
   if (pool[n - 1]->hi == n) pool[n - 1]->hi = 1;
   freenode = n;
-}
-
-void unique_digit_per_col(int d, int col) {
-  int list[10];
-  for (int i = 0; i < 9; i++) {
-    list[i] = 81 * i + 9 * col + d;
-  }
-  list[9] = -1;
-  contains_exactly_one(list);
-}
-
-void unique_digit_per_3x3(int d, int row, int col) {
-  int list[10];
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      list[i * 3 + j] = 81 * (i + 3 * row) + 9 * (j + 3 * col) + d;
-    }
-  }
-  list[9] = -1;
-  contains_exactly_one(list);
 }
 
 void intersect(uint32_t z0, uint32_t z1) {
@@ -432,75 +315,53 @@ int main() {
   pool[1]->hi = 1;
   freenode = 2;
 
-  // The universe is {1, ..., 9^3 = 729}.
-  // Number rows and columns from 0. Digits are integers [1..9].
-  // The digit d at (r, c) is represented by element 81 r + 9 c + d.
+  int v = 1;
+  darray_t board[8][8];
+  // Initialize board and record mononimos.
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      darray_init(board[i][j]);
+      //darray_append(board[i][j], (void *) v++);
+    }
+  }
+  // Dominos.
+  for (int i = 0; i < 8; i++) {
+    for (int j = 1; j < 8; j++) {
+      darray_append(board[i][j - 1], (void *) v);
+      darray_append(board[i][j], (void *) v++);
+      darray_append(board[j - 1][i], (void *) v);
+      darray_append(board[j][i], (void *) v++);
+    }
+  }
+
+  vmax = v - 1;
+
+  /*
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      void print_it(void *data) {
+	int i = (int) data;
+	printf(" %d", i);
+      }
+      darray_forall(board[i][j], print_it);
+      printf("\t\t");
+    }
+    printf("\n");
+  }
+  */
+
   uint32_t k0 = freenode;
-
-  darray_t list;
-  darray_init(list);
-  for(int i = 0; i < 9; i++) {
-    for(int j = 0; j < 9; j++) {
-      int c = getchar();
-      if (EOF == c) {
-	fprintf(stderr, "unexpected EOF\n");
-	exit(1);
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      void print_it(void *data) {
+	int i = (int) data;
+	printf(" %d", i);
       }
-      if ('\n' == c) {
-	fprintf(stderr, "unexpected newline\n");
-	exit(1);
-      }
-      if (c >= '1' && c <= '9') {
-	darray_append(list, (void *) (81 * i + 9 * j + c - '0'));
-      }
-    }
-    int c = getchar();
-    if (EOF == c) {
-      fprintf(stderr, "unexpected EOF\n");
-      exit(1);
-    }
-    if ('\n' != c) {
-      fprintf(stderr, "expected newline\n");
-      exit(1);
-    }
-  }
-  darray_append(list, (void *) -1);
-  contains_all((int *) list->item);
-  darray_clear(list);
-
-  uint32_t k1 = freenode;
-  global_one_digit_per_box();
-  intersect(k0, k1);
-
-  // Number of ways you can put nine 1s into a sudoku is
-  //   9*6*3*6*3*4*2*2.
-  for (int i = 1; i <= 9; i++) {
-    printf("rows %d\n", i);
-    fflush(stdout);
-    for (int r = 0; r < 9; r++) {
+      darray_forall(board[i][j], print_it);
+      printf("\n");
       uint32_t k1 = freenode;
-      unique_digit_per_row(i, r);
-      intersect(k0, k1);
-    }
-  }
-  for (int i = 1; i <= 9; i++) {
-    for (int r = 0; r < 3; r++) {
-      for (int c = 0; c < 3; c++) {
-	printf("3x3 %d: %d, %d\n", i, r, c);
-	fflush(stdout);
-	uint32_t k1 = freenode;
-	unique_digit_per_3x3(i, r, c);
-	intersect(k0, k1);
-      }
-    }
-  }
-  for (int i = 1; i <= 9; i++) {
-    for (int c = 0; c < 9; c++) {
-      printf("cols %d: %d\n", i, c);
-      fflush(stdout);
-      uint32_t k1 = freenode;
-      unique_digit_per_col(i, c);
-      intersect(k0, k1);
+      contains_exactly_one(board[i][j]);
+      if (k0 != k1) intersect(k0, k1);
     }
   }
 
