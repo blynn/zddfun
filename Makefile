a3b2c2c71357d96f9b7312c98886fb65448ad966
Ben Lynn <benlynn@gmail.com>
2009-05-24 03:30:50 -0700

    Can solve sudokus.

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
-CFLAGS:=-lgmp -Wall -std=gnu99
+CFLAGS:=-lgmp -Wall -std=gnu99 -O2 -ltcmalloc
 
-zdd: zdd.c cbt.c
+zdd: zdd.c cbt.c darray.c
--- zdd.c
+++ zdd.c
@@ -3,29 +3,30 @@
 #include <stdio.h>
 #include <gmp.h>
 #include "cbt.h"
+#include "darray.h"
 
 struct node_s {
   uint16_t v;
-  uint16_t lo, hi;
+  uint32_t lo, hi;
 };
 typedef struct node_s *node_ptr;
 typedef struct node_s node_t[1];
 
-node_t pool[1<<16];
-uint16_t freenode;
-mpz_ptr count[1<<16];
+node_t pool[1<<24];
+uint32_t freenode, POOL_MAX = (1<<24) - 1;
+mpz_ptr count[1<<24];
 
-void set_node(uint16_t n, uint16_t v, uint16_t lo, uint16_t hi) {
+void set_node(uint32_t n, uint16_t v, uint32_t lo, uint32_t hi) {
   pool[n]->v = v;
   pool[n]->lo = lo;
   pool[n]->hi = hi;
 }
 
-void pool_swap(uint16_t x, uint16_t y) {
+void pool_swap(uint32_t x, uint32_t y) {
   struct node_s tmp = *pool[y];
   *pool[y] = *pool[x];
   *pool[x] = tmp;
-  for(uint16_t i = 2; i < freenode; i++) {
+  for(uint32_t i = 2; i < freenode; i++) {
     if (pool[i]->lo == x) pool[i]->lo = y;
     else if (pool[i]->lo == y) pool[i]->lo = x;
     if (pool[i]->hi == x) pool[i]->hi = y;
@@ -34,7 +35,7 @@ void pool_swap(uint16_t x, uint16_t y) {
 }
 
 // Count elements in ZDD rooted at node n.
-void get_count(uint16_t n) {
+void get_count(uint32_t n) {
   if (count[n]) return;
   count[n] = malloc(sizeof(mpz_t));
   mpz_init(count[n]);
@@ -42,8 +43,8 @@ void get_count(uint16_t n) {
     mpz_set_ui(count[n], n);
     return;
   }
-  uint16_t x = pool[n]->lo;
-  uint16_t y = pool[n]->hi;
+  uint32_t x = pool[n]->lo;
+  uint32_t y = pool[n]->hi;
   if (!count[x]) get_count(x);
   if (!count[y]) get_count(y);
   mpz_add(count[n], count[x], count[y]);
@@ -230,9 +231,9 @@ void unique_digit_per_row(int d, int r) {
 }
 
 // Construct ZDD of sets containing exactly 1 of the elements in the given list
-// is present in the set. The list is terminated by -1.
+// The list is terminated by -1.
 // Generalized version of previous function.
-void unique_digit_in_boxes(int *list) {
+void contains_exactly_one(int *list) {
   // The order is determined by sorting by number, then by letter.
   int n = freenode;
   int v = 1;
@@ -280,16 +281,50 @@ void unique_digit_in_boxes(int *list) {
   freenode = n;
 }
 
+// Construct ZDD of sets containing all elements in the given list.
+// The list is terminated by -1.
+void contains_all(int *list) {
+  int n = freenode;
+  int v = 1;
+  int *next = list;
+  while (v <= 729) {
+    if (v == *next) {
+      next++;
+      set_node(n, v, 0, n + 1);
+      n++;
+    } else {
+      set_node(n, v, n + 1, n + 1);
+      n++;
+    }
+    v++;
+  }
+  // Fix 729.
+  pool[n - 1]->lo = 1;
+  pool[n - 1]->hi = 1;
+  freenode = n;
+}
+
 void unique_digit_per_col(int d, int col) {
   int list[10];
   for (int i = 0; i < 9; i++) {
     list[i] = 81 * i + 9 * col + d;
   }
   list[9] = -1;
-  unique_digit_in_boxes(list);
+  contains_exactly_one(list);
 }
 
-void intersect(uint16_t z0, uint16_t z1) {
+void unique_digit_per_3x3(int d, int row, int col) {
+  int list[10];
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      list[i * 3 + j] = 81 * (i + 3 * row) + 9 * (j + 3 * col) + d;
+    }
+  }
+  list[9] = -1;
+  contains_exactly_one(list);
+}
+
+void intersect(uint32_t z0, uint32_t z1) {
   struct node_template_s {
     uint16_t v;
     // NULL means this template have been instantiated.
@@ -300,7 +335,7 @@ void intersect(uint16_t z0, uint16_t z1) {
       cbt_it hi;
       // During template instantiation we set n to the pool index
       // of the newly created node.
-      uint16_t n;
+      uint32_t n;
     };
   };
   typedef struct node_template_s *node_template_ptr;
@@ -324,12 +359,12 @@ void intersect(uint16_t z0, uint16_t z1) {
   cbt_t node_tab;
   cbt_init(node_tab);
 
-  cbt_it insert_template(uint16_t k0, uint16_t k1) {
-    uint16_t key[2];
+  cbt_it insert_template(uint32_t k0, uint32_t k1) {
+    uint32_t key[2];
     key[0] = k0;
     key[1] = k1;
     cbt_it it;
-    int just_created = cbt_it_insert_u(&it, tab, (void *) key, 4);
+    int just_created = cbt_it_insert_u(&it, tab, (void *) key, 8);
     if (!just_created) return it;
     if (!k0 || !k1) {
       cbt_it_put(it, bot);
@@ -344,8 +379,12 @@ void intersect(uint16_t z0, uint16_t z1) {
     if (n0->v == n1->v) {
       node_template_ptr t = malloc(sizeof(*t));
       t->v = n0->v;
-      t->lo = insert_template(n0->lo, n1->lo);
-      t->hi = insert_template(n0->hi, n1->hi);
+      if (n0->lo == n0->hi && n1->lo == n0->hi) {
+	t->lo = t->hi = insert_template(n0->lo, n1->lo);
+      } else {
+	t->lo = insert_template(n0->lo, n1->lo);
+	t->hi = insert_template(n0->hi, n1->hi);
+      }
       cbt_it_put(it, t);
       return it;
     } else if (n0->v < n1->v) {
@@ -360,7 +399,7 @@ void intersect(uint16_t z0, uint16_t z1) {
   }
 
   void dump(void* data, const char* key) {
-    uint16_t *n = (uint16_t *) key;
+    uint32_t *n = (uint32_t *) key;
     if (!data) {
       printf("NULL %d:%d\n", n[0], n[1]);
       return;
@@ -370,42 +409,42 @@ void intersect(uint16_t z0, uint16_t z1) {
       printf("%d:%d = (%d)\n", n[0], n[1], t->n);
       return;
     }
-    uint16_t *l = (uint16_t *) cbt_it_key(t->lo);
-    uint16_t *h = (uint16_t *) cbt_it_key(t->hi);
+    uint32_t *l = (uint32_t *) cbt_it_key(t->lo);
+    uint32_t *h = (uint32_t *) cbt_it_key(t->hi);
     printf("%d:%d = %d:%d, %d:%d\n", n[0], n[1], l[0], l[1], h[0], h[1]);
   }
 
-  uint16_t get_node(uint16_t v, uint16_t lo, uint16_t hi) {
+  uint32_t get_node(uint16_t v, uint32_t lo, uint32_t hi) {
     // Create or return existing node representing !v ? lo : hi.
-    uint16_t key[3];
-    key[0] = v;
-    key[1] = lo;
-    key[2] = hi;
+    uint32_t key[3];
+    key[0] = lo;
+    key[1] = hi;
+    key[2] = v;
     cbt_it it;
-    int just_created = cbt_it_insert_u(&it, node_tab, (void *) key, 6);
+    int just_created = cbt_it_insert_u(&it, node_tab, (void *) key, 12);
     if (just_created) {
       cbt_it_put(it, (void *) freenode);
       node_ptr n = pool[freenode];
       n->v = v;
       n->lo = lo;
       n->hi = hi;
-      if (!(freenode % 10000)) printf("freenode = %d\n", freenode);
-      if (0xffff== freenode) {
+      if (!(freenode % 100000)) printf("freenode = %d\n", freenode);
+      if (POOL_MAX == freenode) {
 	fprintf(stderr, "pool is full\n");
 	exit(1);
       }
       return freenode++;
     }
-    return (uint16_t) cbt_it_data(it);
+    return (uint32_t) cbt_it_data(it);
   }
 
-  uint16_t instantiate(cbt_it it) {
+  uint32_t instantiate(cbt_it it) {
     node_template_ptr t = (node_template_ptr) cbt_it_data(it);
     // Return if already converted to node.
     if (!t->lo) return t->n;
     // Recurse on LO, HI edges.
-    uint16_t lo = instantiate(t->lo);
-    uint16_t hi = instantiate(t->hi);
+    uint32_t lo = instantiate(t->lo);
+    uint32_t hi = instantiate(t->hi);
     // Remove HI edges pointing to FALSE.
     if (!hi) {
       t->lo = NULL;
@@ -413,7 +452,7 @@ void intersect(uint16_t z0, uint16_t z1) {
       return lo;
     }
     // Convert to node.
-    uint16_t r = get_node(t->v, lo, hi);
+    uint32_t r = get_node(t->v, lo, hi);
     t->lo = NULL;
     t->n = r;
     return r;
@@ -422,11 +461,11 @@ void intersect(uint16_t z0, uint16_t z1) {
   insert_template(z0, z1);
   freenode = z0;  // Overwrite input trees.
   //cbt_forall(tab, dump);
-  uint16_t key[2];
+  uint32_t key[2];
   key[0] = z0;
   key[1] = z1;
-  cbt_it it = cbt_it_at_u(tab, (void *) key, 4);
-  uint16_t root = instantiate(it);
+  cbt_it it = cbt_it_at_u(tab, (void *) key, 8);
+  uint32_t root = instantiate(it);
   if (root < z0) {
     *pool[z0] = *pool[root];
   } else if (root > z0)  {
@@ -434,12 +473,30 @@ void intersect(uint16_t z0, uint16_t z1) {
   }
   void clear_it(void* data, const char* key) {
     node_template_ptr t = (node_template_ptr) data;
-    uint16_t *k = (uint16_t *) key;
+    uint32_t *k = (uint32_t *) key;
     if (k[0] == k[1] && t != top && t != bot) free(t);
   }
   cbt_forall(tab, clear_it);
   cbt_clear(tab);
 
+  cbt_clear(node_tab);
+}
+
+void check_reduced() {
+  cbt_t node_tab;
+  cbt_init(node_tab);
+  for (uint32_t i = 2; i < freenode; i++) {
+    cbt_it it;
+    uint32_t key[3];
+    key[0] = pool[i]->lo;
+    key[1] = pool[i]->hi;
+    key[2] = pool[i]->v;
+    if (!cbt_it_insert_u(&it, node_tab, (void *) key, 12)) {
+      printf("duplicate: %d %d\n", i, (int) it->data);
+    } else {
+      it->data = (void *) i;
+    }
+  }
   cbt_clear(node_tab);
 }
 
@@ -456,38 +513,77 @@ int main() {
   // The universe is {1, ..., 9^3 = 729}.
   // Number rows and columns from 0. Digits are integers [1..9].
   // The digit d at (r, c) is represented by element 81 r + 9 c + d.
-  uint16_t k0 = freenode;
-  /*
-  one_digit_per_box(0, 0);
-  uint16_t k1 = freenode;
-  one_digit_per_box(0, 1);
-  intersect(k0, k1);
-  k1 = freenode;
-  one_digit_per_box(0, 2);
-  intersect(k0, k1);
-  k1 = freenode;
-  one_digit_per_box(0, 3);
-  intersect(k0, k1);
-  */
+  uint32_t k0 = freenode;
 
-  int i;
+  darray_t list;
+  darray_init(list);
+  for(int i = 0; i < 9; i++) {
+    for(int j = 0; j < 9; j++) {
+      int c = getchar();
+      if (EOF == c) {
+	fprintf(stderr, "unexpected EOF\n");
+	exit(1);
+      }
+      if ('\n' == c) {
+	fprintf(stderr, "unexpected newline\n");
+	exit(1);
+      }
+      if (c >= '1' && c <= '9') {
+	darray_append(list, (void *) (81 * i + 9 * j + c - '0'));
+      }
+    }
+    int c = getchar();
+    if (EOF == c) {
+      fprintf(stderr, "unexpected EOF\n");
+      exit(1);
+    }
+    if ('\n' != c) {
+      fprintf(stderr, "expected newline\n");
+      exit(1);
+    }
+  }
+  darray_append(list, (void *) -1);
+  contains_all((int *) list->item);
+  darray_clear(list);
+
+  uint32_t k1 = freenode;
   global_one_digit_per_box();
-  for (i = 1; i <= 1; i++) {
+  intersect(k0, k1);
+
+  // Number of ways you can put nine 1s into a sudoku is
+  //   9*6*3*6*3*4*2*2.
+  for (int i = 1; i <= 9; i++) {
     printf("rows %d\n", i);
-    for (int r = 0; r < 0; r++) {
-      uint16_t k1 = freenode;
+    fflush(stdout);
+    for (int r = 0; r < 9; r++) {
+      uint32_t k1 = freenode;
       unique_digit_per_row(i, r);
       intersect(k0, k1);
     }
-    printf("cols %d\n", i);
-    for (int c = 7; c < 9; c++) {
-      uint16_t k1 = freenode;
+  }
+
+  for (int i = 1; i <= 9; i++) {
+    for (int r = 0; r < 3; r++) {
+      for (int c = 0; c < 3; c++) {
+	printf("3x3 %d: %d, %d\n", i, r, c);
+	fflush(stdout);
+	uint32_t k1 = freenode;
+	unique_digit_per_3x3(i, r, c);
+	intersect(k0, k1);
+      }
+    }
+  }
+  for (int i = 1; i <= 9; i++) {
+    for (int c = 0; c < 9; c++) {
+      printf("cols %d: %d\n", i, c);
+      fflush(stdout);
+      uint32_t k1 = freenode;
       unique_digit_per_col(i, c);
       intersect(k0, k1);
     }
   }
 
-  for(i = k0; i < freenode; i++) {
+  for(int i = k0; i < freenode; i++) {
     printf("I%d: !%d ? %d : %d\n", i, pool[i]->v, pool[i]->lo, pool[i]->hi);
   }
 
