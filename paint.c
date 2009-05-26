c63ecc75891ecee559cf4b0fcc4f8a12f52860dc
Ben Lynn <benlynn@gmail.com>
2009-05-26 02:48:05 -0700

    Variable renames.

--- paint.c
+++ paint.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <gmp.h>
 #include "cbt.h"
 #include "darray.h"
@@ -64,34 +65,34 @@ uint32_t add_node(uint32_t v, int offlo, int offhi) {
   return freenode++;
 }
 
-void add_row_clue(int *a, int size) {
-  uint32_t table[15 + 1][15 + 1];
-  uint32_t add_row(uint32_t v, int *a, int count, int sum) {
-    int max = 1 + 15 - count - sum + 1 - v + 1;
-    if (table[v][count] != 0) return table[v][count];
+uint32_t add_row_clue(int row, int *a, int size, int root) {
+  uint32_t v = row * 15 + 1;
+  uint32_t table[15][15 + 1];
+  uint32_t partial_row(uint32_t i, int *a, int count, int sum) {
+    int max = 1 + 15 - count - sum + 1 - i;
+    if (table[i][count] != 0) return table[i][count];
     uint32_t first, last;
-    first = last = table[v][count] = add_node(v, 0, 1);
+    first = last = table[i][count] = add_node(v + i, 0, 1);
     for(int j = 1; j < *a; j++) {
-      last = add_node(v + j, 0, 1);
+      last = add_node(v + i + j, 0, 1);
     }
     if (1 == count) {
-      pool[last]->hi = 1;
+      pool[last]->hi = root;
     } else {
-      pool[last]->hi = add_row(v + *a + 1, a + 1, count - 1, sum - *a);
+      pool[last]->hi = partial_row(i + *a + 1, a + 1, count - 1, sum - *a);
     }
-    for(int i = 1; i < max; i++) {
-      first = pool[first]->lo = add_row(v + i, a, count, sum);
+    for(int j = 1; j < max; j++) {
+      first = pool[first]->lo = partial_row(i + j, a, count, sum);
     }
-    printf("A %d %d\n", v, count);
-    return table[v][count];
+    return table[i][count];
   }
 
   int sum = 0;
   for (int i = 0; i < size; i++) {
-    for (int v = 1; v <= 15; v++) table[v][i + 1] = 0;
+    for (int j = 0; j < 15; j++) table[j][i + 1] = 0;
     sum += a[i]; 
   }
-  add_row(1, a, size, sum);
+  return partial_row(0, a, size, sum);
 }
 
 void intersect(uint32_t z0, uint32_t z1) {
@@ -309,10 +310,33 @@ int main() {
     printf("\n");
   }
   */
+  int row_clue[15][15 + 1];
+  for(int i = 0; i < 15; i++) {
+    char s[80];
+    if (!fgets(s, 80, stdin)) {
+      fprintf(stderr, "input error\n");
+      exit(1);
+    }
+    char *w = s;
+    for(int j = 0; j < 15; j++) {
+      if (!sscanf(w, "%d", &row_clue[i][j + 1])) {
+	row_clue[i][0] = j;
+	break;
+      }
+      w = strchr(w + 1, ' ');
+      if (!w) {
+	row_clue[i][0] = j + 1;
+	break;
+      }
+    }
+  }
 
   uint32_t k0 = freenode;
-  int a[] = { 3, 3, 4 };
-  add_row_clue(a, sizeof(a)/sizeof(*a));
+  uint32_t root = 1;
+  for(int i = 15 - 1; i >= 0; i--) {
+    root = add_row_clue(i, &row_clue[i][1], row_clue[i][0], root);
+  }
+  pool_swap(k0, root);
 
   for(int i = k0; i < freenode; i++) {
     printf("I%d: !%d ? %d : %d\n", i, pool[i]->v, pool[i]->lo, pool[i]->hi);
